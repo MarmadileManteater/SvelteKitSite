bc49000b851a7677a615a9a4645ad31752965807
Emma <MarmadileManteater@proton.me>
2023-01-11 18:57:47 -0500

    Using `readFile` instead of `import`

--- src/scripts/set-environment.ts
+++ src/scripts/set-environment.ts
@@ -1,6 +1,6 @@
 
-import packageJSON from '../../package.json'
-import { writeFileSync } from 'fs'
+import { writeFileSync, readFileSync } from 'fs'
 
+const packageJSON = JSON.parse(readFileSync('./package.json').toString())
 packageJSON.environment = process.argv[2]
 writeFileSync('./package.json', JSON.stringify(packageJSON, null, 2))
