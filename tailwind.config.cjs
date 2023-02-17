10d078e2ec6243e7da59fece5f1181ebef99b1d6
Emma <MarmadileManteater@proton.me>
2023-02-17 09:06:49 -0500

    Update tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}','./src/**/**/*.{html,js,svelte,ts}','./src/**/**/**/*.{html,js,svelte,ts}', './data/**/*.html'],
+  content: ['./src/*.{html,js,svelte,ts}', './src/**/*.{html,js,svelte,ts}','./src/**/**/*.{html,js,svelte,ts}','./src/**/**/**/*.{html,js,svelte,ts}', './data/**/*.html'],
   theme: {
     extend: {},
   },
