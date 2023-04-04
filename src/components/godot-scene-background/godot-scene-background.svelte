6fe24596107aca48692654759ba9732148c14b20
Emma <MarmadileManteater@proton.me>
2023-04-03 20:08:07 -0400

    Fix z-index of link in godot-scene-background

--- src/components/godot-scene-background/godot-scene-background.svelte
+++ src/components/godot-scene-background/godot-scene-background.svelte
@@ -2,7 +2,7 @@
   import Emoji from "../emoji/emoji.svelte"
   export let sceneUrl: string
 </script>
-<span class='z-10 top-2 left-0 md:absolute'>
+<span class='z-10 relative top-2 left-0 md:absolute'>
   <a href='https://github.com/MarmadileManteater/JustThreeBlocksBackground' rel='noreferrer' target='_blank' class='pt-2 pl-2 hover:underline text-white'>Background</a> <a class='pt-2 hover:underline text-white' href='https://godotengine.org/'>made with <Emoji emoji='🤖' />Godot</a>
 </span>
 <iframe 
