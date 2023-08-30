7de5407521036c50548c3db63d36650e3769e5d1
Emma <MarmadileManteater@proton.me>
2023-08-30 07:33:30 -0400

    Remove CSS from `social-posts.svelte`

--- src/components/social-posts/social-posts.svelte
+++ src/components/social-posts/social-posts.svelte
@@ -4,22 +4,10 @@
 
   export let socialPosts: ISocialPost[] = [];
 </script>
-<div class="flex flex-wrap justify-center">
+<div class="flex flex-wrap justify-center gap-[25px]">
   {#each socialPosts as post, i}
-    <div class="social-post flex w-[100%]">
+    <div class="social-post flex lg:basis-[300px] basis-full">
       <SocialPost {post} />
     </div>
   {/each}
 </div>
-
-<style scoped>
-  .social-post {
-    flex-basis: calc(33% - 15px);
-  }
-
-  @media(max-width:1024px) {
-    .social-post {
-      flex-basis: 100%;
-    }
-  }
-</style>
