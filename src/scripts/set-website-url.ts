0227963fb5e66e9b4c3938f5dce454f206d3bfef
Emma <MarmadileManteater@proton.me>
2023-08-30 17:15:12 -0400

    Add script for setting website url and add optimize images to CI

--- src/scripts/set-website-url.ts
+++ src/scripts/set-website-url.ts
@@ -2,6 +2,5 @@
 import { writeFileSync, readFileSync } from 'fs'
 
 const packageJSON = JSON.parse(readFileSync('./package.json').toString())
-packageJSON.environment = process.argv[2]
-packageJSON.adapter = "node"
+packageJSON.domain = process.argv[2]
 writeFileSync('./package.json', JSON.stringify(packageJSON, null, 2))
