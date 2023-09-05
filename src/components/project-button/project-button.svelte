10d0e0c343d72724fbc3d46117082bb20e5e7aa5
Emma <MarmadileManteater@proton.me>
2023-09-05 12:23:37 -0400

    Add a tiny amount of visual flair to the button component

--- src/components/project-button/project-button.svelte
+++ src/components/project-button/project-button.svelte
@@ -6,5 +6,12 @@
 <a
   href={link}
   rel="noreferrer"
-  class={`p-3 inline-block hover:underline dark:bg-zinc-700 ${index % 2 === 0?'bg-blue-600':'bg-blue-900'}  text-white dark:bg-zinc-600 rounded-lg mr-5 mb-3`}
-><slot /> &raquo;</a>
+  class={`p-3 inline-block hover:underline dark:bg-zinc-700 ${index % 2 === 0?'bg-blue-600':'bg-blue-800'}  text-white rounded-lg mr-5 mb-3 relative z-[1] pb-5`}
+>
+  <div 
+    class="{index % 2 === 0?'bg-blue-500':'bg-blue-700'} dark:bg-zinc-600 w-full absolute h-12 rounded-lg z-[-1] top-0 left-0"
+  ></div>
+  <slot /> &raquo;
+</a>
+<style scoped>
+</style>
