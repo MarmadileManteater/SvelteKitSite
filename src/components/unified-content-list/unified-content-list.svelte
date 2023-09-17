87a891f4ca750b63d06932fd9cfbdf2b428230c4
Emma <MarmadileManteater@proton.me>
2023-09-16 22:33:19 -0400

    Make star count not take up entire line
    - Absolutely position it on small displays

--- src/components/unified-content-list/unified-content-list.svelte
+++ src/components/unified-content-list/unified-content-list.svelte
@@ -10,52 +10,54 @@
   export let startIndex : number = 0
 </script>
 {#each content as contentItem, i}
-  {#if contentItem.type === 'IProject'}
-    <ContentCard
-      title={contentItem.title}
-      titleLink={contentItem.buttons[0].link}
-      summary={contentItem.summary}
-      thumbnail={contentItem.thumbnail}
-      tags={contentItem.tags}
-      tagData={tagData}
-      index={startIndex + i}
-    >
-      <span
-        slot='inline-w-tags'
+  <div class='relative'>
+    {#if contentItem.type === 'IProject'}
+      <ContentCard
+        title={contentItem.title}
+        titleLink={contentItem.buttons[0].link}
+        summary={contentItem.summary}
+        thumbnail={contentItem.thumbnail}
+        tags={contentItem.tags}
+        tagData={tagData}
+        index={startIndex + i}
       >
-        {#if contentItem.ghStars > 0}
-          <a
-            href={`https://github.com/${contentItem.ghFullName}/stargazers`}
-            class='hover:underline pr-[10px] inline-block align-top hover:underline p-[7px] bg-zinc-200 dark:bg-zinc-700 dark:text-white rounded-xl mr-3 mb-2 mt-2 inline-block max-h-[110px]'
-          >
-            <Emoji emoji='🌠'/> {contentItem.ghStars}
-          </a>
-        {/if}
-      </span>
-      {#each contentItem.buttons as button, k}
-        <ProjectButton
-          {...button}
-          index={k}
-        >{button.prefix} <strong>{button.locationName}</strong></ProjectButton>
-      {/each}
+        <span
+          slot='inline-w-tags'
+        >
+          {#if contentItem.ghStars > 0}
+            <a
+              href={`https://github.com/${contentItem.ghFullName}/stargazers`}
+              class='hover:underline pr-[10px] inline-block align-top hover:underline p-[7px] bg-zinc-200 dark:bg-zinc-700 dark:text-white rounded-xl mr-3 mb-2 mt-2 inline-block max-h-[110px] sm:top-0 sm:right-0 sm:relative absolute top-2 right-2'
+            >
+              <Emoji emoji='🌠'/> {contentItem.ghStars}
+            </a>
+          {/if}
+        </span>
+        {#each contentItem.buttons as button, k}
+          <ProjectButton
+            {...button}
+            index={k}
+          >{button.prefix} <strong>{button.locationName}</strong></ProjectButton>
+        {/each}
+      </ContentCard>
+    {/if}
+    {#if contentItem.type === 'IBlogPost'}
+      <ContentCard
+        title={contentItem.title}
+        titleLink={`/blog/${contentItem.id}`}
+        summary={contentItem.shortDescription}
+        tags={contentItem.tags}
+        tagData={tagData}
+        index={startIndex + i}
+      >
+        <p
+          class='pb-2 text-zinc-500 dark:text-zinc-400'
+        >
+          <em>
+            Last updated {new Date(contentItem.gittime).toLocaleDateString('en-GB', { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZone: 'GMT' })} GMT
+          </em>
+        </p>
     </ContentCard>
-  {/if}
-  {#if contentItem.type === 'IBlogPost'}
-    <ContentCard
-      title={contentItem.title}
-      titleLink={`/blog/${contentItem.id}`}
-      summary={contentItem.shortDescription}
-      tags={contentItem.tags}
-      tagData={tagData}
-      index={startIndex + i}
-    >
-      <p
-        class='pb-2 text-zinc-500 dark:text-zinc-400'
-      >
-        <em>
-          Last updated {new Date(contentItem.gittime).toLocaleDateString('en-GB', { year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZone: 'GMT' })} GMT
-        </em>
-      </p>
-  </ContentCard>
-  {/if}
+    {/if}
+  </div>
 {/each}
